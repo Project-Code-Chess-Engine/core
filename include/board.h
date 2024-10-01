9b5c20c284fad981e4b46e2bb9ee0312007f41d7
joshuachen <jhc3628@gmail.com>
2024-09-30 21:05:35 -0500

    Declare functions and structs

--- include/board.h
+++ include/board.h
@@ -1,6 +1,52 @@
 #pragma once
 #include <array>
 #include <cstdint>
+#include <memory>
+
+// extern "C" is used here to allow non-c++ programs to use these sturcts and functions
+extern "C" {
+    // sets up all of the tables
+    void initialize();
+
+    // an enum to represent the different boards
+    enum Piece {
+        WHITE, BLACK, PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING, ENPASSANT
+    };
+
+    // the struct that stores the boards
+    typedef struct {
+        // the bitboards
+        uint64_t boards[9];
+    } Board;
+};
+
+// a magic square
+struct SMagic {
+    // the moves that can be made
+    std::unique_ptr<uint64_t[]> moves;
+    // the occupancy mask
+    uint64_t mask;
+    // the mask representing the square
+    uint64_t square;
+    // the number of significant bits
+    uint8_t bits;
+    // the magic number (hard part is finding it)
+    uint8_t magic;
+};
+
+// table to store the rookMagics
+static std::array<std::array<SMagic, 8>, 8> rookMagics;
+// table to store the bishopMagics
+static std::array<std::array<SMagic, 8>, 8> bishopMagics;
+
+// generate the SMagics for rooks
+void findRookMagics();
+
+// generate the SMagics for bishops
+void findBishopMagics();
+
+// use the SMagic to get moves
+void useMagic(const Board& board, const SMagic& magic);
 
 // table to store moves for knights
 static std::array<std::array<uint64_t, 8>, 8> knightTable;
--- src/board.cpp
+++ src/board.cpp
@@ -1,5 +1,17 @@
 #include "board.h"
 
+void findRookMagics() {
+    // Use a bitboard to go through all positions and genreate the SMagic
+}
+
+void findBishopMagics() {
+    // Use a bitboard to go through all positions and genreate the SMagic
+}
+
+void useMagic(const Board& board, const SMagic& magic) {
+    // Use the SMagic on the board to calculate the moves
+}
+
 void generateKnightTable() {
     // Use a bitboard to go through all of the positions and generate the places that the knight is able to move.
 }
